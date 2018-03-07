20e7ef2f2b3239ed721eefbc5d71832123660df5
Sean Travers <117412476@umail.ucc.ie>
2018-03-07 20:30:33 +0000

    styling

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -2,37 +2,83 @@
 // Start the session
 session_start();
 ?>
-
 <!DOCTYPE html>
+
 <html>
     <head>
-        <title>Enter Details</title>
+        <title>ebus1</title>
+        <link rel="stylesheet" href="mystylesheet.css" type="text/css" />
+     
+        <script type="text/javascript" src="ebus2_validator.js"></script>
+        <script type="text/javascript" src="cost_calc.js"></script>
         
+        <!-- Add icon library -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script type="text/javascript" src="ebus2_validator.js"></script>
+        
     </head>
+    
     <body>
-        <h4>Please enter your payment details</h4>
+   <header>
+		<div id = "Title">
+       
+		<h1 style="Color:black;">Payment Details </h1>
+     
+		</div>	
+   </header>
+   
+	<nav>
+   
+		
+	</nav>
+	
+	
+	<div class="ebuscontainer">
+		<section id="main">
+			
+		<!-- Form --> 	
+		 <h4>Enter payment details</h4>
         
         
-            <form action="Ebus3.php" method="POST">
+            <form method="POST" action="Ebus3.php">
+            	
+            	
+            		<!-- Input Textboxes --> 
+            		<label class="align" for="name">Name</label>
+            		<input type="text" id="name" name="name">
+            		<br/>
+            		
+            		<label class="align" for="email">Email</label>
+            		<input type="text" id="email" name="email">
+            		<br/>
 
-                    <label for="user_pin">PIN</label>
-                    
+                    <label class="align" for="user_pin">PIN</label>
                     <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4">
-
-                <button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
+                    <br/>
+					
+					<!-- Buttons --> 
+                	<button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
               
             </form>
             
-            <br>
-            <button onclick="validateDetails()">Validate</button>
+            		<br/>
             
-            <?php
-            // Set session variables
-            $_SESSION["total"] = $_POST["total"];
-            ?>
+            		<button class="validate" onClick="validateDetails()">Validate</button>
+            
+            		<?php
+            		// Set session variables
+        			$_SESSION["total"] = $_POST["total"];
+           			?>
+            
+
+        	
+		</section>
+		
+		<!-- Basket --> 
+		<p id="rectext">In Basket:</p>
+     	<div id="reclogo"></div>
         
+	</div>
     </body>
 </html>
--- ebusiness/Ebus3.php
+++ ebusiness/Ebus3.php
@@ -3,15 +3,31 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>RECEIPT</title>
-    </head>
-    <body>
-        <h4>RECEIPT</h4>
-                
+    
+		<head>
+        <title>ebus1</title>
+        <link rel="stylesheet" href="mystylesheet.css" type="text/css" />
+     
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Receipt --> 
+	<div class="ebuscontainer">
+		<section id="main">
+			<h2>Receipt:</h2>
+			
+             
         <?php
         // Echo session variables that were set on previous page
-        echo "Total is " . $_SESSION["total"] . ".";
+        echo "Name: " . $_POST["name"] . ". <br>";
+        echo "Email: " . $_POST["email"] . "<br>";
+        echo "Total is &euro;" . $_SESSION["total"] . ". <br>";
         ?>
+        <p>Thank you for your purchase.</p>
+        
+        
+        </section>
+        
+        
+    </div>
+	
     </body>
 </html>
